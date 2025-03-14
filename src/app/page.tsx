a140ce84a352c2797fa80e78903467e6b7ba1a09
System Administrator <haseeb.salman1234@outlook.com>
2025-03-15 00:54:46 +0500

    integrated more sanity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ import GuideToBook from "@/components/page-components/guide-to-book";
 export default async function Home() {
 
   const homeData: { content: [PagesContent] } = await client.fetch({
-    query: `*[_type=='LTL'][0]{
+    query: `*[_type=='home'][0]{
   "content":[...contentBlocks[]->{
     ...,
     "images":[...images[]{"imageUrl":asset->url}],
