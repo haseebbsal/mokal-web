0b9de1a5737f06c72ba630d0e916a94353a28e90
System Administrator <haseeb.salman1234@outlook.com>
2025-03-20 23:17:08 +0500

    fix prod

--- src/components/page-components/navbar.tsx
+++ src/components/page-components/navbar.tsx
@@ -29,14 +29,14 @@ export default async function MainNavbar() {
                     </Marquee>}
 
                     <Navbar height={'auto'} position="static" className={`justify-start items-center bg-nav  text-base-blue sm:px-8 px-2 py-4 sm:py-0 !h-auto min-h-16`} classNames={{ wrapper: "w-full max-w-full p-0 sm:flex-row flex-col" }} shouldHideOnScroll>
-                        <NavbarContent className=" flex" >
-                            <div className="flex w-full sm:gap-16 gap-8 items-center">
-                                <div className="flex gap-4">
+                        <NavbarContent className=" flex w-full" >
+                            <div className="flex w-full sm:gap-16 gap-8 sm:justify-start justify-between items-center">
+                                <div className="flex sm:gap-4 gap-2">
                                     <p className="sm:block hidden">www.mgcfreight.com</p>
-                                    {configData.countries?.slice(0, 3).map((e, index) => <p key={e.name} className={`${index != 0 ? "border-l-2" : "sm:border-l-2"}  pl-2 border-base-blue`}>{e.name}</p>)}
+                                    {configData.countries?.slice(0, 3).map((e, index) => <p key={e.name} className={`${index != 0 ? "border-l-2" : "sm:border-l-2"} text-xs sm:text-sm pl-2 border-base-blue`}>{e.name}</p>)}
                                 </div>
-                                <div className="flex gap-2 items-center">
-                                    <IoIosCall size={20} />
+                                <div className="flex gap-2 sm:text-sm text-xs items-center">
+                                    <IoIosCall  />
                                     <p>{configData.phoneNumber}</p>
                                 </div>
                             </div>
