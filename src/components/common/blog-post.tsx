cb4624880a72ccc2294c4cc94becc0a927143801
Haseeb Ahmed <haseeb.ahmed@mgcfreight.com>
2026-06-22 11:47:16 -0700

    [fix] Disbaled date in testimonials

--- src/components/common/blog-post.tsx
+++ src/components/common/blog-post.tsx
@@ -4,26 +4,26 @@ import Image from "next/image"
 import { FaQuoteRight } from "react-icons/fa"
 import ReactStars from "react-stars"
 
-interface BlogPostProp{
-    index:number
-    rating:number
-    description:string
-    name:string,
-    imageUrl:string,
-    createdAt:string
+interface BlogPostProp {
+    index: number
+    rating: number
+    description: string
+    name: string,
+    imageUrl: string,
+    createdAt: string
 }
 
-export default function BlogPost({index,description,rating,name,imageUrl,createdAt}:BlogPostProp) {
+export default function BlogPost({ index, description, rating, name, imageUrl, createdAt }: BlogPostProp) {
     return (
         <div className="px-8">
-            <div className={`p-4 ${index%2==0?'text-white bg-base-blue': 'text-base-blue bg-white'} items-start text-start shadow-xl  rounded-xl flex flex-col gap-2`}>
+            <div className={`p-4 ${index % 2 == 0 ? 'text-white bg-base-blue' : 'text-base-blue bg-white'} items-start text-start shadow-xl  rounded-xl flex flex-col gap-2`}>
                 <div className="flex justify-between w-full">
                     <Image src={imageUrl} alt="person" height={70} width={70} />
                     <FaQuoteRight className="text-base-purple text-2xl" />
                 </div>
                 <div>
                     <p className="font-semibold">{name}</p>
-                    <p className="text-xs text-text-gray">{new Date(createdAt).toDateString()}</p>
+                    {/* <p className="text-xs text-text-gray">{new Date(createdAt).toDateString()}</p> */}
                 </div>
                 <ReactStars
                     edit={false}
