3ee9d94183a79d151745a2982885f8721f21d128
Haseeb Ahmed <haseeb.ahmed@mgcfreight.com>
2026-06-22 11:11:58 -0700

    [fix] Production dataset change

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -1,1 +1,4 @@
-export const isProduction = process.env.SANITY_STUDIO_DATASET === "production";
+// Don't get confused, it's staging for production and for test/development use "test" dataset
+// So if dataset is "staging" it's production and if "test" it's test/development
+
+export const isProduction = process.env.SANITY_STUDIO_DATASET === "staging";
